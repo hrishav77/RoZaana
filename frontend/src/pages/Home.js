41342eadf33905477ddd62985f154a96113b6fae
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-05-21 14:01:54 +0530

    mapped the goals
    put fetch request and mapped it in frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,25 @@
-import React from 'react'
-import { Text} from '@chakra-ui/react'
+// import React from 'react'
+// import { Text} from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [goals,setGoals]=useState(null)
+  useEffect(()=>{
+    const fetchGoals=async()=>{
+    const data=await fetch('/api/goals')
+    const goaljson=await data.json()
+    if(data.ok){
+      setGoals(goaljson)
+    }
+   }
+   fetchGoals()
+  },[])
   return (
-    <Text p={5} fontSize="lg">Home</Text>
+    <div className="goals">
+      {goals && goals.map((goal)=>(
+        <p key={goal._id}>{goal.goaltitle}</p>
+      ))}
+      </div>
+    
   )
 }
